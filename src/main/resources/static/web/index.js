09ddceda6d46103efe03076aca156555eccadbd2
AdrianoBalbo <adriano.balbo00@gmail.com>
2022-10-05 11:36:23 -0300

    Arreglando un bug que impedía logearse / registrarse

--- src/main/resources/static/web/index.js
+++ src/main/resources/static/web/index.js
@@ -23,17 +23,25 @@ createApp({
         toggleSignUpIn() {
             this.logContainer.classList.toggle("sign-up-mode")
         },
-        login() {
-            axios.post('/api/login', `email=${this.userMail}&password=${this.userPassword}`, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
-                .then(res => {
-                    window.location.href = "./accounts.html"
-                    console.log("funciona pai");
-                })
-                .catch(() => swal("Wrong email or password. Try again."))
+        // login() {
+        //     axios.post('/api/login', `email=${this.userMail}&password=${this.userPassword}`, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
+        //         .then(res => {
+        //             window.location.href = "./accounts.html"
+        //             console.log("funciona pai");
+        //         })
+        //         .catch(() => swal("Wrong email or password. Try again."))
+
+        // },
+        login(){
+            axios.post('/api/login', `email=${this.userMail}&password=${this.userPassword}`)
+            .then(response => {
+                window.location.href = "./accounts.html"
+            })
+            .catch(()=> swal("Wrong email or password. Try again"))
 
         },
         register() {
-            axios.post('/api/client', `firstName=${this.newFirstName}&lastName=${this.newLastName}&email=${this.newEmail}&password=${this.newPassword}`, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
+            axios.post('/api/client', `firstName=${this.newFirstName}&lastName=${this.newLastName}&email=${this.newEmail}&password=${this.newPassword}`)
                 .then(res => axios.post('/api/login', `email=${this.newEmail}&password=${this.newPassword}`, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
                     .then(res => {
                         axios.post('/api/client/current/accounts/', `accountType=SAVING`, { headers: { 'content-type': 'application/x-www-form-urlencoded' } })
